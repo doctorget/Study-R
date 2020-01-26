471fea0220c5da0171ca876c1a24194977d84aee
getsam <samuelsoares177778@gmail.com>
2020-01-26 16:01:49 -0300

    terminando os conceitos stats 1

--- Estatistica I - R/9.3.Distribuição t de Student Aula R.R
+++ Estatistica I - R/9.3.Distribuição t de Student Aula R.R
@@ -1,6 +1,10 @@
-#Formacao Cientista de Dados - Fernando Amaral
+#Formacao Cientista de Dados
 
-pt(1.5, 8)
+# Media 75 amostra 9 e dp 10 
+# qual a prob do salario de um data ser menor que 80
+# < 80 consulta na tabela t = 1,5
+pt(1.5, 8) # 8 são os graus de liberdade
 
 
+# mesmo problema só que a prob é maior que
 pt(1.5,8,lower.tail=F)

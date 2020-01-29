4efe3984afd616f55ddeb759e1a55377a3cb0fc8
getsam <samuelsoares177778@gmail.com>
2020-01-29 12:16:01 -0300

    finalizando modulo estatistica 2

--- Estatistica II R_/9. logistica.R
+++ Estatistica II R_/9. logistica.R
@@ -1,17 +1,27 @@
-#Formacao Cientista de Dados - Fernando Amaral
-eleicao = read.csv(file.choose(),sep=';',header=T)
-plot(eleicao$DESPESAS,eleicao$SITUACAO) 
+#Formacao Cientista de Dados
+
+eleicao = read.csv(file.choose(),sep=';',header=T) #Abrindo base
+
+plot(eleicao$DESPESAS,eleicao$SITUACAO) #comparando duas variaveis
+
 summary(eleicao)
 
-cor(eleicao$DESPESAS,eleicao$SITUACAO)
+cor(eleicao$DESPESAS,eleicao$SITUACAO) # verificando se existe uma correlação entre as despesas e a candidatura
 
-modelo = glm(SITUACAO~DESPESAS,data=eleicao,family="binomial") 
+modelo = glm(SITUACAO~DESPESAS,data=eleicao,family="binomial") # glm gera outro tipo de modelo que pode ser passado por parametro
+# neste caso é para umadistribuição binomial
+
 summary(modelo)
 
-plot(eleicao$DESPESAS,eleicao$SITUACAO,col='red',pch=20)
-points(eleicao$DESPESAS, modelo$fitted, pch=4) 
+plot(eleicao$DESPESAS,eleicao$SITUACAO,col='red',pch=20) # mesmo grafico só mudando a cor dos pontos 
+
+points(eleicao$DESPESAS, modelo$fitted, pch=4) # grafico mostrando a pontos ajustado pelo modelo mostrando a probabilidade de ser eleito baseado no investimento
 
 prevereleicao = read.csv(file.choose(),sep=';',header=T)
-prevereleicao$RESULT = predict(modelo,newdata=prevereleicao,type="response") 
+
+prevereleicao$RESULT = predict(modelo,newdata=prevereleicao,type="response") # prevendo proxima eleição baseado nos dados de novos candidatos e seu investimento
 prevereleicao$RESULT
+
+fix(prevereleicao$RESULT) # coloca os dados em uma table melhorando vizualização
+
 predic(modelo, as.data.frame(DESPESAS = 1000))
